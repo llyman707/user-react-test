d623c2f177220598133d044b1097ca28d5595906
llyman707 <llyman707@gmail.com>
2025-11-01 09:02:54 -0400

    adding more functionality to user form test

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -12,3 +12,29 @@ test('it shows two inputs and a button', () => {
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
 });
+
+test('it calls onUserAdd when form is submitted', async () => {
+    const mock = jest.fn();
+    //try to render component
+    render(<UserForm onUserAdd={mock}/>);
+    //find the two inputs
+    const nameInput = screen.getByRole('textbox', {
+        name: /name/i,
+    });
+    const emailInput = screen.getByRole('textbox', {
+        name: /email/i,
+    });
+    //simulate typing in an name
+    await user.click(nameInput);
+    await user.keyboard('jane');
+    //simulate typing in an email
+    await user.click(emailInput);
+    await user.keyboard('jane@jane.com');
+    //find the button
+    const button = screen.getByRole('button');
+    //simulate clicking the button
+    await user.click(button);
+    //assertion to make sure 'onUserAdd' gets called with eamil/name
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledWith({name:'jane', email:'jane@jane.com'});
+});
